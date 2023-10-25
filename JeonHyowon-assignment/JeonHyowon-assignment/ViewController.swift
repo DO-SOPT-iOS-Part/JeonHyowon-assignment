5c7edd72ef3cbc79b71f336ce238a387e2bdb13e
Hyowon Jeon <hyowon612@gmail.com>
2023-10-25 16:33:57 +0900

    프로젝트 세팅

---
+++ JeonHyowon-assignment/JeonHyowon-assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JeonHyowon-assignment
+//
+//  Created by Hyowon Jeon on 10/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JeonHyowon-assignment/Pods/Target Support Files/Pods-JeonHyowon-assignment/Pods-JeonHyowon-assignment-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JeonHyowon_assignment : NSObject
+@end
+@implementation PodsDummy_Pods_JeonHyowon_assignment
+@end
